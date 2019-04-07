06c992417a331a9e8fc4c0e81508ccccc910fc06
James <metal.lunchbox@gmail.com>
2019-04-07 11:37:18 +0800

    add lesson number resolver

--- src/resolvers/ClassSession.js
+++ src/resolvers/ClassSession.js
@@ -4,5 +4,26 @@ exports.ClassSession = {
   },
   points (root, args, context) {
     return context.prisma.classSession({ id: root.id }).points()
+  },
+  async number (root, args, context) {
+    const group = await context.prisma.classSession({ id: root.id }).group()
+    const semester = await context.prisma
+      .classSession({ id: root.id })
+      .group()
+      .semester()
+    const lessons = await context.prisma
+      .classSessionsConnection({
+        where: {
+          group: {
+            id: group.id
+          },
+          startsAt_gte: semester.startDate,
+          endsAt_lte: semester.endDate,
+          startsAt_lt: root.startsAt
+        }
+      })
+      .aggregate()
+      .count()
+    return lessons + 1
   }
 }
