1a1f8b4bd8cd11213669f94499ba679154af5882
James <metal.lunchbox@gmail.com>
2019-04-12 20:28:07 +0800

    add classSession subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,7 +1,23 @@
 exports.Subscription = {
+  classSession: {
+    subscribe: async (_, { id }, { prisma }) => {
+      return prisma.$subscribe.classSession({ where: { node: id } }).node()
+    },
+    resolve: payload => {
+      return payload
+    }
+  },
+  classSessions: {
+    subscribe: async (_, { where }, { prisma }) => {
+      return prisma.$subscribe.classSession({ where })
+    },
+    resolve: payload => {
+      return payload
+    }
+  },
   semesters: {
     subscribe: async (_, args, { prisma }) => {
-      prisma.$subscribe
+      return prisma.$subscribe
         .semester({
           where: {
             mutation_in: ['CREATED', 'UPDATED']
