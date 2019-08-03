86e3ecae47ec18294a24c16ea059acdc4d618340
James <metal.lunchbox@gmail.com>
2019-08-03 10:17:04 +0800

    add tag resolvers

--- src/resolvers/Mutation/lesson.js
+++ src/resolvers/Mutation/lesson.js
@@ -1,8 +1,11 @@
 exports.lesson = {
-  createLesson(_, { input }, { prisma }) {
+  createLesson (_, { input }, { prisma }) {
     return prisma.createLesson({ ...input })
   },
-  updateLesson(_, { input, id }, { prisma }) {
+  updateLesson (_, { input, id }, { prisma }) {
     return prisma.updateLesson({ data: input, where: { id } })
+  },
+  deleteLesson (_, { id }, { prisma }) {
+    return prisma.deleteLesson({ id })
   }
 }
