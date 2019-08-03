cbcd68ebb5478b26e6389658f4675c6c712db049
James <metal.lunchbox@gmail.com>
2019-08-03 11:32:52 +0800

    tags connection count resolver

--- src/resolvers/Tag.js
+++ src/resolvers/Tag.js
@@ -7,5 +7,17 @@ exports.Tag = {
   },
   words (root, _, { prisma }) {
     return prisma.tag({ id: root.id }).words()
+  },
+  async connections (root, _, { prisma }) {
+    const lessonCount = await prisma.lessonsConnection({
+      where: { tags_some: { id: root.id } }
+    }).aggregate().count()
+    const materialsCount = await prisma.materialsConnection({
+      where: { tags_some: { id: root.id } }
+    }).aggregate().count()
+    const wordsCount = await prisma.wordsConnection({
+      where: { tags_some: { id: root.id } }
+    }).aggregate().count()
+    return lessonCount + materialsCount + wordsCount
   }
 }
