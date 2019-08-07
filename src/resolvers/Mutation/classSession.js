1a2358d76cd6d676e6caa610edc18cbc2811f5ad
James <metal.lunchbox@gmail.com>
2019-08-07 16:36:31 +0800

    add lesson to createClassSession enable report rel

--- src/resolvers/Mutation/classSession.js
+++ src/resolvers/Mutation/classSession.js
@@ -7,7 +7,7 @@ const {
 } = require('../validation/classSession')
 
 exports.classSession = {
-  async createClassSession (_, { groupId, input }, context) {
+  async createClassSession (_, { groupId, input, lessonId }, context) {
     await Promise.all([
       withinYear(input.startsAt, input.endsAt),
       checkOrder(input.startsAt, input.endsAt, null, context),
@@ -16,11 +16,8 @@ exports.classSession = {
     ])
     return context.prisma.createClassSession({
       ...input,
-      group: {
-        connect: {
-          id: groupId
-        }
-      }
+      group: { connect: { id: groupId } },
+      lesson: { connect: { id: lessonId } }
     })
   },
   // to avoid running every validation
@@ -34,7 +31,7 @@ exports.classSession = {
       }
     })
   },
-  async updateClassSession (_, { id, input, groupId }, context) {
+  async updateClassSession (_, { id, input, groupId, lessonId }, context) {
     await Promise.all([
       withinYear(input.startsAt, input.endsAt),
       checkOrder(input.startsAt, input.endsAt, id, context),
@@ -42,28 +39,48 @@ exports.classSession = {
       duringGroupSemester(input.startsAt, input.endsAt, groupId, context),
       checkConflicts(input.startsAt, input.endsAt, id, context)
     ])
-    if (groupId) {
-      return context.prisma.updateClassSession({
-        data: {
-          ...input,
-          group: { connect: { id: groupId } }
-        },
-        where: { id }
-      })
-    }
+    const data = input
+    if (groupId) input.group = { connect: { id: groupId } }
+    if (lessonId) input.lesson = { connect: { id: lessonId } }
+    return context.prisma.updateClassSession({ data, where: { id } })
+  },
+  addLessonToClassSession (_, { id, lessonId }, context) {
     return context.prisma.updateClassSession({
-      data: input,
+      data: { lesson: { connect: { id: lessonId } } },
       where: { id }
     })
   },
   deleteClassSession (_, { id }, context) {
     return context.prisma.deleteClassSession({ id })
   },
-  createReport (_, { sessionId, input }, context) {
-    return context.prisma.updateSession({
-      where: { id: sessionId },
-      data: { report: { create: input } }
+  async createReport (_, { classSessionId }, context) {
+    const lesson = await context.prisma.classSession({ id: classSessionId }).lesson()
+    const words = await context.prisma.lesson({ id: lesson.id }).words()
+    const wordIds = words.map(w => {
+      return { id: w.id }
     })
+    const materials = await context.prisma.lesson({ id: lesson.id }).materials()
+    const materialIds = materials.map(m => {
+      return { id: m.id }
+    })
+    if (!lesson) {
+      throw new Error('You must add a lesson to the session before creating a report.')
+    }
+    const { homeworkEN, homeworkZH, summaryEN, summaryZH } = lesson
+    const data = {
+      report: {
+        create: {
+          homeworkZH,
+          homeworkEN,
+          summaryEN,
+          summaryZH,
+          published: false,
+          words: { connect: wordIds },
+          materials: { connect: materialIds }
+        }
+      }
+    }
+    return context.prisma.updateClassSession({ data, where: { id: classSessionId } })
   },
   updateReport (_, { sessionId, input }, context) {
     return context.prisma.updateSession({
