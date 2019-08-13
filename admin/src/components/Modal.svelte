a3d9e33c412b4a4a61b942360a849d069d50fb29
James <metal.lunchbox@gmail.com>
2019-08-13 20:26:34 +0800

    fix hidden modal contents on mobile

--- admin/src/components/Modal.svelte
+++ admin/src/components/Modal.svelte
@@ -3,7 +3,9 @@
 
   export let open = false
 
-  const close = () => { open = false }
+  const close = () => {
+    open = false
+  }
 </script>
 
 <style>
@@ -20,12 +22,20 @@
 </style>
 
 {#if open}
-  <div class="modal is-active">
-    <div class="modal-background" on:click={close} transition:fade|local="{{ duration: 200 }}"></div>
-    <div class="modal-content is-clipped" in:fly="{{ duration: 200, y: -300 }}" out:fly="{{ duration: 200, y: 300 }}">
-      <section class="section">
-       <slot></slot>
-      </section>
-    </div>
+<div class="modal is-active">
+  <div
+    class="modal-background"
+    on:click="{close}"
+    transition:fade|local="{{ duration: 200 }}"
+  ></div>
+  <div
+    class="modal-content"
+    in:fly="{{ duration: 200, y: -300 }}"
+    out:fly="{{ duration: 200, y: 300 }}"
+  >
+    <section class="section">
+      <slot></slot>
+    </section>
   </div>
-  {/if}
+</div>
+{/if}
