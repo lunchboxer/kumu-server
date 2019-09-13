6bebe86d5c8b41a117594b1bebf430b2352f12e5
James <metal.lunchbox@gmail.com>
2019-09-13 16:08:02 +0800

    hidden logout function to unset localstorage

---
+++ parents/src/components/Logout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { student } from '../data/stores'
+  import { push } from 'svelte-spa-router'
+
+  student.logout()
+  push('/')
+</script>
--- parents/src/routes.js
+++ parents/src/routes.js
@@ -1,9 +1,11 @@
 import NotFound from './components/NotFound.svelte'
 import Home from './components/Home.svelte'
 import ClassSession from './components/ClassSession.svelte'
+import Logout from './components/Logout.svelte'
 
 export default {
   '/': Home,
   '/session/:id': ClassSession,
+  '/logout': Logout,
   '*': NotFound
 }
