784a5c6d29529672136e3840753b9d7c7240b19f
James <metal.lunchbox@gmail.com>
2020-01-21 15:00:47 +0800

    Add support for other material types

--- parents/src/components/MaterialList.svelte
+++ parents/src/components/MaterialList.svelte
@@ -7,6 +7,8 @@
   const images = materials.filter(material => material.type.split('/')[0] === 'image')
   const audios = materials.filter(material => material.type.split('/')[0] === 'audio')
   const videos = materials.filter(material => material.type.split('/')[0] === 'video')
+  const otherMaterials = materials.filter(material => !['image', 'audio', 'video']
+    .includes(material.type.split('/')[0]))
 
   const thumbUrl = url => {
     const size = 150
@@ -84,4 +86,24 @@
       {/each}
     </ul>
   {/if}
+
+  {#if otherMaterials.length > 0}
+  <ul>
+    {#each otherMaterials as material (material.id)}
+    <li class="materialitem">
+      <div class="itemtitle">
+        <a href={material.url}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>
+          <span>{material.title}</span>
+        </a>
+      </div>
+      {#if material.notes}
+        <div class="notes">
+          {@html marked(material.notes, { breaks: true })}
+        </div>
+      {/if}
+    </li>
+    {/each}
+  </ul>
+{/if}
 {/if}
